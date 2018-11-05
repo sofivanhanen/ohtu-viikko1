a84ec6c1d0bd40d8318e788fe34a8ce5c4402024
sofivanhanen <sofia.vanhanen0@gmail.com>
2018-11-05 18:27:57 +0200

    Tests for NHLStatistics

---
+++ NhlStatistics1/src/test/java/ohtuesimerkki/StatisticsTest.java
@@ -0,0 +1,55 @@
+package ohtuesimerkki;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class StatisticsTest {
+    
+    Reader readerStub = new Reader() {
+ 
+        public List<Player> getPlayers() {
+            List<Player> players = new ArrayList<Player>();
+ 
+            players.add(new Player("Semenko", "EDM", 4, 12));
+            players.add(new Player("Lemieux", "PIT", 45, 54));
+            players.add(new Player("Kurri",   "EDM", 37, 53));
+            players.add(new Player("Yzerman", "DET", 42, 56));
+            players.add(new Player("Gretzky", "EDM", 35, 89));
+ 
+            return players;
+        }
+    };
+ 
+    Statistics stats;
+
+    @Before
+    public void setUp(){
+        // luodaan Statistics-olio joka käyttää "stubia"
+        stats = new Statistics(readerStub);
+    }
+    
+    @Test
+    public void playerIsFound() {
+        assertEquals("EDM", stats.search("Semenko").getTeam());
+    }
+    
+    @Test
+    public void playerIsNotFound() {
+        assertEquals(null, stats.search("Sofia"));
+    }
+    
+    @Test
+    public void teamIsFound() {
+        assertEquals(3, stats.team("EDM").size());
+    }
+    
+    @Test
+    public void topScorersAreFound() {
+        assertEquals(3, stats.topScorers(3).size());
+    }
+}
