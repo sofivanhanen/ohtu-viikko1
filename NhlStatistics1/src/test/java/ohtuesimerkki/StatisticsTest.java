79032adf32582e65ca432aa2b871026c181a2bd8
sofivanhanen <sofia.vanhanen0@gmail.com>
2018-11-09 22:10:25 +0200

    Remove unused dependancies

--- NhlStatistics1/src/test/java/ohtuesimerkki/StatisticsTest.java
+++ NhlStatistics1/src/test/java/ohtuesimerkki/StatisticsTest.java
@@ -2,10 +2,7 @@ package ohtuesimerkki;
 
 import java.util.ArrayList;
 import java.util.List;
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 public class StatisticsTest {
